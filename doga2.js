217fd4349108e022caee84161cba8304dbeb7469
Patrick0601 <100844354+Patrik0601@users.noreply.github.com>
2024-11-19 08:48:52 +0100

    Add files via upload

---
+++ doga2.js
@@ -0,0 +1,23 @@
+//1.feladat
+function bontas(a) {
+    const eredeti = []
+    for (let i = 0; i < a.length; i++){
+        eredeti.push(a[i] + " ")
+    }
+    return eredeti
+}
+console.log(bontas(420))
+
+//2.feladat
+
+//3.feladat
+
+//4.feladat
+function faktorial(n){
+    let sum = number;
+    for (let i = 1 ; i != n; i--){
+        sum = sum*(i-1)
+    }
+    return sum
+}
+console.log(faktorial(7))
